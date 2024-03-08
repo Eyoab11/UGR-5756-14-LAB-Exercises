9eeec75abc9c620bd5658f387e2a4f66cf18b45a
Eyoab11 <134016127+Eyoab11@users.noreply.github.com>
2024-03-08 16:10:04 +0300

    Add files via upload
    LAB-1-Variables-and-types

---
+++ LAB-1-Variables-and-types.dart
@@ -0,0 +1,9 @@
+main() {
+  String name = "Eyoab Amare";
+  int age = 21;
+  String favorite_color = "Orange";
+
+  print('My name is $name');
+  print('I am $age years old');
+  print('My favourite color is $favorite_color');
+}
